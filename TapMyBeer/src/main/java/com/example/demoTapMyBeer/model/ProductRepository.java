e1fdf6c517776da956abff8daa0de6b137706336
Joel Giladi <joel@giladi.dev>
2021-02-17 15:45:10 -0800

    Adding Product, Categories, Order, and Warehouse Repositories

---
+++ TapMyBeer/src/main/java/com/example/demoTapMyBeer/model/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.demoTapMyBeer.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	List<Product> findByName(String name);
+
+	List<Product> findByInventory(int numInventory);
+	
+}
