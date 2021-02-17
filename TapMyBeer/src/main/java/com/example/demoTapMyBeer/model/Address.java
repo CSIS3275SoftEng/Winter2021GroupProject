2d8b1fad9c7b197f34ee25b28e8026d7a678eae6
sophi <sophi@DESKTOP-V6VUFJG>
2021-02-17 00:16:30 -0800

    address class

--- TapMyBeer/src/main/java/com/example/demoTapMyBeer/model/Address.java
+++ TapMyBeer/src/main/java/com/example/demoTapMyBeer/model/Address.java
@@ -1,5 +1,44 @@
 package com.example.demoTapMyBeer.model;
 
-public class Address {
 
+	import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "addresses")
+	public class Address {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private long addId;
+		
+		@Column(name = "AddressNo")
+		private int addNo;
+		
+		@Column(name = "Street")
+		private String street;
+		
+		@Column(name = "City")
+		private String city;
+		
+		@Column(name = "Province")
+		private String province;
+		
+		@Column(name = "PostalCode")
+		private String postalCode;
+		
+		public Address() {}
+		
+		public Address(int addNo, String street, String city, String province, String postalCode ) {
+			this.addNo =addNo;
+			this.street= street;
+			this.city = city;
+			this.province = province;
+			this.postalCode= postalCode;
+			
+		}
 }
