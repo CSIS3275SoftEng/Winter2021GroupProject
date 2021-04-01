6fd7635e5a3d2737246a12933318472a50193f43
Andrea Cruz <abjaudcruza@student.douglascollege.ca>
2021-04-01 13:23:37 -0700

    Adjustments to CORS compliance

--- demoTapMyBeer/src/main/java/com/example/demoTapMyBeer/controller/ProductBySellerController.java
+++ demoTapMyBeer/src/main/java/com/example/demoTapMyBeer/controller/ProductBySellerController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.example.demoTapMyBeer.model.Seller;
 import com.example.demoTapMyBeer.model.SellerRepository;
 import com.example.demoTapMyBeer.response.MessageResponse;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class ProductBySellerController {
